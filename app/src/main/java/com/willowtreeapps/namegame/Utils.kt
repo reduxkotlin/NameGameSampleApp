542bde4c55a8d76e4cd9ccfa43a1351ae63e6988
Patrick Jackson <patjackson52@gmail.com>
2019-04-01 18:40:50 -0400

    add shake animation and red color to incorrect answer

--- app/src/main/java/com/willowtreeapps/namegame/Utils.kt
+++ app/src/main/java/com/willowtreeapps/namegame/Utils.kt
@@ -1,8 +1,22 @@
 package com.willowtreeapps.namegame
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
 import android.content.res.Resources
 
 val Int.dp: Int
     get() = (this / Resources.getSystem().displayMetrics.density).toInt()
 val Int.px: Int
     get() = (this * Resources.getSystem().displayMetrics.density).toInt()
+
+/*
+ * Convenience function for executing lambda after animation set is finished.
+ */
+fun AnimatorSet.onComplete(after: () -> Unit) {
+        this.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator?) {
+                after()
+            }
+        })
+    }
