fec4d5886eb80f303b2651fcd108e49b41a0c77b
Patrick Jackson <patjackson52@gmail.com>
2019-04-05 15:39:50 -0400

    fix game results not showing

--- common/src/commonMain/kotlin/com/willowtreeapps/common/Reducers.kt
+++ common/src/commonMain/kotlin/com/willowtreeapps/common/Reducers.kt
@@ -1,6 +1,5 @@
 package com.willowtreeapps.common
 
-import com.beyondeye.reduks.ReducerFn
 import com.willowtreeapps.common.Actions.*
 import com.willowtreeapps.common.repo.Profile
 import com.willowtreeapps.common.util.TimeUtil
@@ -8,43 +7,41 @@ import kotlin.math.abs
 import kotlin.random.Random
 
 /**
- * Reducers and functions used by reducers are in this file.  Functions must be pure functiuns without
+ * Reducers and functions used by reducers are in this file.  Functions must be pure functions without
  * side effects.
  */
-
-val reducer = ReducerFn<AppState> { state, action ->
-    when (action) {
-        is FetchingProfilesStartedAction -> state.copy(isLoadingProfiles = true)
-        is FetchingProfilesSuccessAction -> {
-            val rounds = generateRounds(action.profiles, state.numQuestions)
-            state.copy(isLoadingProfiles = false, profiles = action.profiles, questions = rounds)
-        }
-        is FetchingProfilesFailedAction -> state.copy(isLoadingProfiles = false, errorLoadingProfiles = true, errorMsg = action.message)
-        is NamePickedAction -> {
-            val status = if (state.currentQuestionProfile().matches(action.name)) {
-                Question.Status.CORRECT
-            } else {
-                Question.Status.INCORRECT
+fun reducer(state: AppState, action: Any): AppState =
+        when (action) {
+            is FetchingProfilesStartedAction -> state.copy(isLoadingProfiles = true)
+            is FetchingProfilesSuccessAction -> {
+                val rounds = generateRounds(action.profiles, state.numQuestions)
+                state.copy(isLoadingProfiles = false, profiles = action.profiles, questions = rounds)
             }
-            val newQuestions = state.questions.toMutableList()
-            newQuestions[state.currentQuestionIndex] = newQuestions[state.currentQuestionIndex].copy(answerName = action.name, status = status)
-            state.copy(questions = newQuestions, waitingForNextQuestion = true)
-        }
-        is NextQuestionAction -> state.copy(waitingForNextQuestion = false, currentQuestionIndex = state.currentQuestionIndex + 1)
-        is GameCompleteAction -> state.copy(waitingForResultsTap = true, waitingForNextQuestion = false, currentQuestionIndex = state.currentQuestionIndex + 1)
-        is StartOverAction, is ResetGameStateAction -> AppState.INITIAL_STATE
-        is StartQuestionTimerAction -> state.copy(questionClock = action.initialValue)
-        is DecrementCountDownAction -> state.copy(questionClock = state.questionClock - 1)
-        is TimesUpAction -> {
-            val status = Question.Status.TIMES_UP
-            val newQuestions = state.questions.toMutableList()
-            newQuestions[state.currentQuestionIndex] = newQuestions[state.currentQuestionIndex].copy(answerName = "", status = status)
-            state.copy(questions = newQuestions, waitingForNextQuestion = true, questionClock = -1)
-        }
+            is FetchingProfilesFailedAction -> state.copy(isLoadingProfiles = false, errorLoadingProfiles = true, errorMsg = action.message)
+            is NamePickedAction -> {
+                val status = if (state.currentQuestionProfile().matches(action.name)) {
+                    Question.Status.CORRECT
+                } else {
+                    Question.Status.INCORRECT
+                }
+                val newQuestions = state.questions.toMutableList()
+                newQuestions[state.currentQuestionIndex] = newQuestions[state.currentQuestionIndex].copy(answerName = action.name, status = status)
+                state.copy(questions = newQuestions, waitingForNextQuestion = true)
+            }
+            is NextQuestionAction -> state.copy(waitingForNextQuestion = false, currentQuestionIndex = state.currentQuestionIndex + 1)
+            is GameCompleteAction -> state.copy(waitingForResultsTap = true, waitingForNextQuestion = false, currentQuestionIndex = state.currentQuestionIndex + 1)
+            is StartOverAction, is ResetGameStateAction -> AppState.INITIAL_STATE
+            is StartQuestionTimerAction -> state.copy(questionClock = action.initialValue)
+            is DecrementCountDownAction -> state.copy(questionClock = state.questionClock - 1)
+            is TimesUpAction -> {
+                val status = Question.Status.TIMES_UP
+                val newQuestions = state.questions.toMutableList()
+                newQuestions[state.currentQuestionIndex] = newQuestions[state.currentQuestionIndex].copy(answerName = "", status = status)
+                state.copy(questions = newQuestions, waitingForNextQuestion = true, questionClock = -1)
+            }
 
-        else -> throw AssertionError("Action ${action::class.simpleName} not handled")
-    }
-}
+            else -> throw AssertionError("Action ${action::class.simpleName} not handled")
+        }
 
 fun generateRounds(profiles: List<Profile>, n: Int): List<Question> =
         profiles.takeRandomDistinct(n)
